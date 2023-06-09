35d3acbf46b5cd2189c403da2ec06117b8ad5a6c
elfill03 <rafaelfillah755@gmail.com>
2023-06-09 22:28:04 +0700

    mengerjakan 3 section terakhir semaksimal mungkin

--- 29_Deployment/Praktikum/react_vite/src/main.jsx
+++ 29_Deployment/Praktikum/react_vite/src/main.jsx
@@ -1,13 +1,17 @@
+import { ApolloProvider } from "@apollo/client";
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App";
 import store from "./Config/redux/store";
+import client from "./apollo-client";
 
 createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>
+  </ApolloProvider>
 );
